635a68e19f123a730e3574b43412ffd3a32d717d
Bulat <118826176+akhmetzyanov-bulat@users.noreply.github.com>
2023-05-16 10:38:08 +0300

    Update spript.sh

--- task1/spript.sh
+++ task1/spript.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 grep 'http://profi.ru/remont/santehnika' /path_to_nginx_log | grep 'GET' | awk '{print $37}' | sort | uniq -c | sort -rn | head -10
 
 # Идет подсчет количество повторений по значению поля $sent_http_content_type и само значение $sent_http_content_type
